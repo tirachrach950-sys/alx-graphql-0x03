da0efff3cbe63314f14e7fe3fe8dfce876304e1c
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-15 02:16:25 +0100

    Update index.tsx

--- alx-rick-and-morty-app/pages/index.tsx
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -1,1 +1,13 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
+import ErrorProneComponent from "@/components/ErrorProneComponent";
+
+const Home: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <ErrorProneComponent />
+    </ErrorBoundary>
+  );
+};
+
+export default Home;
 
