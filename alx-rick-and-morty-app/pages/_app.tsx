c29087e6d652720176da6536e092c647370fb0e6
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-15 02:13:47 +0100

    Update _app.tsx

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,1 +1,13 @@
+import type { AppProps } from "next/app";
+import ErrorBoundary from "@/components/ErrorBoundary";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
 
