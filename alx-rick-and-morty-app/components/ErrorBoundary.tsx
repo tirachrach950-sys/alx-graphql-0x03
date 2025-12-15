c1d678588465650c4379ecb9c5ea8ebb3be4e992
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-15 02:13:16 +0100

    Update ErrorBoundary.tsx

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,1 +1,51 @@
+import React, { ReactNode } from "react";
+
+interface State {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+          <h2>Oops, there is an error!</h2>
+          <button
+            onClick={() => this.setState({ hasError: false })}
+            style={{
+              marginTop: "1rem",
+              padding: "0.5rem 1rem",
+              backgroundColor: "#22c55e",
+              color: "white",
+              borderRadius: "6px",
+            }}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
 
